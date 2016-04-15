5dbf1513b0507a57373fc5d7280c7ec0a5dafdab
andrejZ7 <andrej.zlnka@gmail.com>
2016-04-15 15:02:24 +0200

    objekt pre reprezentovanie pouzivatela

---
+++ src/main/java/com/andrej/nacitaniedat/model/Pouzivatel.java
@@ -0,0 +1,48 @@
+package com.andrej.nacitaniedat.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pouzivatel")
+public class Pouzivatel implements Serializable {
+  
+  @Id
+  @GeneratedValue
+  private Integer id;
+  private Integer katalogoveId;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the katalogoveId
+     */
+    public Integer getKatalogoveId() {
+        return katalogoveId;
+    }
+
+    /**
+     * @param katalogoveId the katalogoveId to set
+     */
+    public void setKatalogoveId(Integer katalogoveId) {
+        this.katalogoveId = katalogoveId;
+    }
+          
+  
+ 
+}
