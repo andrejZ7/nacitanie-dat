635b4c0ac6527770b1fd39ff379109fe77923ab3
andrejZ7 <andrej.zlnka@gmail.com>
2016-04-15 15:02:15 +0200

    objekt pre reprezentovanie knihy

---
+++ src/main/java/com/andrej/nacitaniedat/model/Kniha.java
@@ -0,0 +1,52 @@
+
+package com.andrej.nacitaniedat.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author andrej
+ */
+
+@Entity
+@Table(name = "kniha")
+public class Kniha implements Serializable {
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String isbn;
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+}
