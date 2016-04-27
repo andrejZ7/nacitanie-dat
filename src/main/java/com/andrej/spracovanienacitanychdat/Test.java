5fe3bcf0c04ceda3c4bc111cbf8834e46f6d21d4
andrejZ7 <andrej.zlnka@gmail.com>
2016-04-27 19:13:41 +0200

    testovacia trieda pre transakcie

---
+++ src/main/java/com/andrej/spracovanienacitanychdat/Test.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.andrej.spracovanienacitanychdat;
+
+import com.andrej.nacitaniedat.PersistenceManager;
+import com.andrej.nacitaniedat.model.Kniha;
+import com.andrej.nacitaniedat.model.Pouzivatel;
+import com.andrej.nacitaniedat.model.Transakcia;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author andre
+ */
+public class Test {
+    
+     public static void main(String [] args) {
+        EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
+        Integer id = 0011333;
+        
+        CriteriaBuilder critBld = em.getCriteriaBuilder();		
+        CriteriaQuery<Pouzivatel> query = critBld.createQuery(Pouzivatel.class);  
+        Root<Pouzivatel> root = query.from(Pouzivatel.class);
+        Pouzivatel pouzivatel;
+             
+        query.where((critBld.equal(root.get("id"), 2545419)));
+        Query qu = em.createQuery(query);        
+        try{
+            pouzivatel = (Pouzivatel) qu.getSingleResult();           
+        }
+        catch(javax.persistence.NoResultException e){			
+            System.out.println("Chyba pri query Transakcie!!!!!!!!!!!!");			
+        }
+        int s = 78;
+        
+        em.close();
+        PersistenceManager.INSTANCE.close();
+        
+     }
+    
+}
