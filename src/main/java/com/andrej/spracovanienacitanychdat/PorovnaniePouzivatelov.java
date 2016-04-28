cffd566458c43be2a8ea63f9c12097fc5e4edb77
andrejZ7 <andrej.zlnka@gmail.com>
2016-04-28 22:36:34 +0200

    trieda pre porovnavanie pouzivatelov

---
+++ src/main/java/com/andrej/spracovanienacitanychdat/PorovnaniePouzivatelov.java
@@ -0,0 +1,40 @@
+package com.andrej.spracovanienacitanychdat;
+
+import com.andrej.nacitaniedat.model.Kniha;
+import com.andrej.nacitaniedat.model.Pouzivatel;
+import com.andrej.spracovaniedat.DataLoader;
+import com.andrej.spracovaniedat.UserServices;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author andrej
+ */
+public class PorovnaniePouzivatelov {
+    
+    public static void main(String [] args) {
+        DataLoader dataLoader = new DataLoader();
+        UserServices userService = new UserServices();
+        List<Pouzivatel> pouzivateliaList = dataLoader.nacitajVsetkychPouzivatelov();
+        int pocitadlo = 0;
+        Pouzivatel userA;
+        Pouzivatel userB;
+        int vsetciPouzivatelia = pouzivateliaList.size();
+        
+        for(int i=0 ; i<vsetciPouzivatelia ; i++){
+            System.out.println(pocitadlo + ". *****************************************************************");
+            pocitadlo++;
+            for(int j=0 ; i<vsetciPouzivatelia ; j++){                
+                userA = pouzivateliaList.get(i);
+                userB = pouzivateliaList.get(j);
+                if(i != j){
+                    userService.porovnajPouzivatelov(userA, userB);
+                }
+            }
+        }
+        
+        
+       
+    }
+}
