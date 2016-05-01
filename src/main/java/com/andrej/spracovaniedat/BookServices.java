f6b115e660f93eb65e786d3b7861a7ea450a1beb
andrejZ7 <andrej.zlnka@gmail.com>
2016-05-01 16:40:11 +0200

    sluzby pre pracu s knihami

---
+++ src/main/java/com/andrej/spracovaniedat/BookServices.java
@@ -0,0 +1,28 @@
+package com.andrej.spracovaniedat;
+
+import Jama.Matrix;
+
+/**
+ *
+ * @author andrej
+ */
+public class BookServices {
+    
+    private double vypocitajSkalarnySucin(Matrix m1, Matrix m2) {
+        int vectorLength = m1.getColumnDimension();
+        double[][] v1 = m1.getArray();                
+        double[][] v2 = m2.getArray();
+        double sum = 0;
+
+        for(int i=0 ; i<vectorLength ; i++){
+            sum += v1[0][i] * v2[0][i];
+        }
+        return sum;
+    }
+    
+    public double vypocitajKosinusVzdialenost(Matrix sourceDoc, Matrix targetDoc) {    
+        double skalarnySucin = vypocitajSkalarnySucin(sourceDoc, targetDoc);
+        double euklidovskaVzdialenost = sourceDoc.normF() * targetDoc.normF();
+        return skalarnySucin / euklidovskaVzdialenost;
+    }
+}
