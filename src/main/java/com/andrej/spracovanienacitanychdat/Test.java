67a55f6f05edaf2e088fd4363f8c5a1e2d2689e4
andrejZ7 <andrej.zlnka@gmail.com>
2016-05-01 23:05:35 +0200

    nejake male testy

--- src/main/java/com/andrej/spracovanienacitanychdat/Test.java
+++ src/main/java/com/andrej/spracovanienacitanychdat/Test.java
@@ -7,13 +7,13 @@ package com.andrej.spracovanienacitanychdat;
 
 import com.andrej.nacitaniedat.PersistenceManager;
 import com.andrej.nacitaniedat.model.Kniha;
-import com.andrej.nacitaniedat.model.Pouzivatel;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
@@ -21,8 +21,24 @@ import javax.persistence.criteria.Root;
  */
 public class Test {
     
+    private static String upravDatumNaCislo(String datum) {
+        if (StringUtils.isNumeric(datum)) {
+            return datum;
+        }
+        else {
+            String pattern = "(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)";
+            String[] dateArray = datum.split(pattern);
+            for (int i=0 ; i<dateArray.length ; i++) {
+                if ( StringUtils.isNumeric(dateArray[i]) ) {
+                    return dateArray[i];
+                }
+            }
+        }
+        return datum;
+    }
+    
      public static void main(String [] args) {
-        EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
+        
       
        /* CriteriaBuilder critBld = em.getCriteriaBuilder();		
         CriteriaQuery<Pouzivatel> query = critBld.createQuery(Pouzivatel.class);  
@@ -51,7 +67,7 @@ public class Test {
         em.close();
         PersistenceManager.INSTANCE.close();*/
                
-        CriteriaBuilder critBld = em.getCriteriaBuilder();		
+        /*CriteriaBuilder critBld = em.getCriteriaBuilder();		
         CriteriaQuery<Pouzivatel> query = critBld.createQuery(Pouzivatel.class);  
         Root<Pouzivatel> root = query.from(Pouzivatel.class);
         Pouzivatel pouzivatel;
@@ -62,7 +78,34 @@ public class Test {
         pouzivatel = (Pouzivatel) qu.getSingleResult();
         
         int sda = 78;
+        */
         
+        List<Kniha> vsetkyKnihy;
+        
+        EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
+        CriteriaBuilder critBld = em.getCriteriaBuilder();		
+        CriteriaQuery<Kniha> query = critBld.createQuery(Kniha.class);  
+        Root<Kniha> root = query.from(Kniha.class);
+        
+        query.select(root);   
+	Query qu = em.createQuery(query);
+	
+        vsetkyKnihy = (List<Kniha>) qu.getResultList();    
+         
+        
+        
+        String upravenyDatum;
+        
+        for (int i=0 ; i<vsetkyKnihy.size() ; i++) {
+            if (vsetkyKnihy.get(i).getDatum() != null) {
+                upravenyDatum = upravDatumNaCislo(vsetkyKnihy.get(i).getDatum());           
+                vsetkyKnihy.get(i).setDatum(upravenyDatum);            
+            }
+        
+        }
+     
+     
+     
      }
     
 }
