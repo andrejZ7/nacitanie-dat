6764a1c8b86ae46e65aca6d66a7e244d541f1c32
andrejZ7 <andrej.zlnka@gmail.com>
2016-05-02 18:20:15 +0200

    CustomComparator pre porovnanie podobnosti knih

---
+++ src/main/java/com/andrej/spracovaniedat/CustomComparator.java
@@ -0,0 +1,20 @@
+package com.andrej.spracovaniedat;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author andrej
+ */
+public class CustomComparator  implements Comparator<PodobneKnihy> {
+
+    @Override
+    public int compare(PodobneKnihy o1, PodobneKnihy o2) {
+        if (o1.getPodobnost() > o2.getPodobnost()) return -1;
+        if (o1.getPodobnost() < o2.getPodobnost()) return 1;
+        return 0;
+    }
+    
+    
+    
+}
