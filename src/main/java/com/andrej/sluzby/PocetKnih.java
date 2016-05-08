e811a3306f789527562f5e57aa970837771393c7
andrejZ7 <andrej.zlnka@gmail.com>
2016-05-08 14:12:03 +0200

    nahrada za staticke premenne pri vypoctoch

---
+++ src/main/java/com/andrej/sluzby/PocetKnih.java
@@ -0,0 +1,26 @@
+package com.andrej.sluzby;
+
+/**
+ *
+ * @author andrej
+ */
+public class PocetKnih {
+    
+    private int pocetOdporucenychKnih = 5;
+    private double percentoPorovnavanychKnih = 80;
+
+    /**
+     * @return the pocetOdporucenychKnih
+     */
+    public int getPocetOdporucenychKnih() {
+        return pocetOdporucenychKnih;
+    }
+    
+    /**
+     * @return the percentoPorovnavanychKnih
+     */
+    public double getPercentoPorovnavanychKnih() {
+        return percentoPorovnavanychKnih;
+    }
+    
+}
